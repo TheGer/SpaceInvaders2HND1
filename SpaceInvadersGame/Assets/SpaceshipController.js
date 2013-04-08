df1e350f7ab23c41403e55f0b445a8434e5ed6b6
Gerry Said <gerrysaid@gmail.com>
2013-04-08 15:50:32 +0200

    Lesson 4
    Added multiple textures when player is hit.
    Added aliens shooting back.

--- SpaceInvadersGame/Assets/SpaceshipController.js
+++ SpaceInvadersGame/Assets/SpaceshipController.js
@@ -2,16 +2,43 @@
 
 var laserSlot:Rigidbody;
 static var score:int;
+//health set to 100
+static var health:int=100;
+
+var colours:Material[];
+
+
+
+function OnTriggerEnter(other:Collider)
+{
+	if (other.gameObject.tag=="enemylaser")
+	{
+		//player was hit, reduce health and change colour
+		this.renderer.material = colours[1];
+		health--;
+	}
+}
+
+function OnTriggerExit(other:Collider)
+{
+	//when the laser leaves the spaceship, set the colour back to default
+	this.renderer.material = colours[0];
+}
+
+
 
 function OnGUI()
 {
 	GUI.color = Color.green;
+	
 	GUI.Label(Rect(0,0,100,25),"Score: "+score);
+	GUI.Label(Rect(0,25,100,25),"Health: "+health);
 }
 
 
 function Start () {
-
+	//by default, the spaceship will be green
+	this.renderer.material = colours[0];
 }
 
 function Update () {
@@ -22,4 +49,10 @@ function Update () {
 	{
 		Instantiate(laserSlot,transform.position,transform.rotation);
 	}
+	
+	if (health <=0)
+	{
+		//game ends
+	}
+	
 }
